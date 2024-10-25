032c7c94e65f7f9314371ca350f3ce22c0b2e703
Carlos Chamizo Cano <49963607+chamix0@users.noreply.github.com>
2024-10-25 02:09:12 +0200

    a

---
+++ Source/SimpleTowerDefense/Private/Managers/TowerWorldManager.cpp
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SimpleTowerDefense/Public/Managers/TowerWorldManager.h"
+
+void UTowerWorldManager::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	
+}
+
+void UTowerWorldManager::Deinitialize()
+{
+	Super::Deinitialize();
+}
---
+++ Source/SimpleTowerDefense/Public/Managers/TowerWorldManager.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "TowerWorldManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLETOWERDEFENSE_API UTowerWorldManager : public UWorldSubsystem
+{
+	GENERATED_BODY()
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+};
